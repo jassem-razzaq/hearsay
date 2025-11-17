aa71cc801cfe02e6fdd50db5d44d6e6fe696a607
Harrison Pham <hpham1997@gmail.com>
2025-11-16 22:58:21 -0500

    add global podcast and podcast-episode avg rating procedures

--- hearsay_procedures2.sql
+++ hearsay_procedures2.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS insert_podcast_review $$
 CREATE PROCEDURE insert_podcast_review(IN user_id_p INT, IN podcast_id_p INT, IN rating_p INT, IN text_p VARCHAR(255))
 BEGIN
-	INSERT INTO podcast_review (user_id, podcast_id, rating, text) 
+    INSERT INTO podcast_review (user_id, podcast_id, rating, text) 
     VALUES (user_id_p, podcast_id_p, rating_p, text_p);
 END $$
 DELIMITER ;
@@ -23,7 +23,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS update_podcast_review $$
 CREATE PROCEDURE update_podcast_review(IN user_id_p INT, IN podcast_id_p INT, IN rating_p INT, IN text_p VARCHAR(255))
 BEGIN
-	UPDATE podcast_review
+    UPDATE podcast_review
     SET rating = rating_p, text = text_p
     WHERE user_id = user_id_p AND podcast_id = podcast_id_p;
 END $$
@@ -41,7 +41,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS insert_podcast_episode_review $$
 CREATE PROCEDURE insert_podcast_episode_review(IN user_id_p INT, IN podcast_id_p INT, IN episode_num_p INT, IN rating_p INT, IN text_p VARCHAR(255))
 BEGIN
-	INSERT INTO episode_review (user_id, podcast_id, episode_num, rating, text) 
+    INSERT INTO episode_review (user_id, podcast_id, episode_num, rating, text) 
     VALUES (user_id_p, podcast_id_p, episode_num_p, rating_p, text_p);
 END $$
 DELIMITER ;
@@ -58,7 +58,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS update_podcast_episode_review $$
 CREATE PROCEDURE update_podcast_episode_review(IN user_id_p INT, IN podcast_id_p INT, IN episode_num_p INT, IN rating_p INT, IN text_p VARCHAR(255))
 BEGIN
-	UPDATE episode_review
+    UPDATE episode_review
     SET rating = rating_p, text = text_p
     WHERE user_id = user_id_p AND podcast_id = podcast_id_p AND episode_num = episode_num_p;
 END $$
@@ -66,3 +66,61 @@ DELIMITER ;
 
 CALL update_podcast_episode_review(51, 1, 1169, 2, "Boring");
 SELECT * FROM episode_review WHERE user_id = 51;
+
+
+
+/*
+Get global podcast average rating
+*/
+DELIMITER $$
+DROP PROCEDURE IF EXISTS get_global_podcast_avg_rating $$
+CREATE PROCEDURE get_global_podcast_avg_rating(IN podcast_id_p INT)
+BEGIN
+	SELECT AVG(rating)
+    FROM podcast 
+	JOIN podcast_review ON podcast.id = podcast_review.podcast_id
+    WHERE podcast.id = podcast_id_p;
+END $$
+DELIMITER ;
+
+CALL get_global_podcast_avg_rating(1);
+
+/*
+Get global podcast avg rating by episode
+*/
+DELIMITER $$
+DROP PROCEDURE IF EXISTS get_global_podcast_episode_avg_rating $$
+CREATE PROCEDURE get_global_podcast_episode_avg_rating(IN podcast_id_p INT)
+BEGIN
+	SELECT AVG(rating)
+    FROM podcast 
+    JOIN episode_review ON podcast.id = episode_review.podcast_id
+    WHERE podcast.id = podcast_id_p;
+END $$
+DELIMITER ;
+
+CALL get_global_podcast_episode_avg_rating(1);
+
+
+
+/*
+Get a user's rating of a podcast
+*/
+DELIMITER $$
+DROP PROCEDURE IF EXISTS get_user_podcast_rating $$
+CREATE PROCEDURE get_user_podcast_rating(IN user_id_p INT, IN podcast_id_p INT)
+BEGIN
+	SELECT rating
+    FROM user
+    JOIN podcast_review ON user.id = podcast_review.user_id
+    WHERE user.id = user_id_p AND podcast_review.podcast_id = podcast_id_p;
+END $$
+DELIMITER ;
+
+CALL get_user_podcast_rating(4, 5);
+
+
+
+/*
+Get a user's avg episode rating for a podcast
+*/
