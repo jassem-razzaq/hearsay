36ba185ed9ed87a72d1ef79a5be59f5dceb433af
jassem-razzaq <jassem.razzaq@gmail.com>
2025-11-24 19:19:21 -0500

    feat: display friends and playlist

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,86 +1,45 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { LoginContext } from "../contexts/LoginContext";
+import Friends from "./Friends";
+import Playlists from "./Playlists";
 
-type Friend = {
+type User = {
   id: string;
-  date_added: string;
   username: string;
   first_name: string;
   last_name: string;
-};
-
-type Playlist = {
-  name: string;
-  description: string;
-};
-
-type Episode = {
-  podcast_id: string;
-  episode_num: string;
+  bio: string;
 };
 
 const API_URL_BASE = import.meta.env.VITE_API_URL;
 
 export default function Profile() {
-  const userID = useParams().userID;
-  async function getUserInfo() {
-    // Profile data
-    const u_response: Response = await fetch(`${API_URL_BASE}/users/${userID}`);
-    const profileData = await u_response.json();
+  const { loggedIn, userID } = useContext(LoginContext);
+  const [profile, setProfile] = useState<User>([]);
+  const urlID = useParams().userID;
 
-    // Friends data (Delete friends)
-    const f_response: Response = await fetch(
-      `${API_URL_BASE}/users/${userID}/friends`
-    );
-    const allFriendsData = await f_response.json();
+  useEffect(() => {
+    async function getUserInfo() {
+      // Profile data
+      const u_response: Response = await fetch(
+        `${API_URL_BASE}/users/${urlID}`
+      );
+      const profileData: User = await u_response.json();
+      setProfile(profileData);
+    }
 
-    // Individual friend data
-    const friendsData = await Promise.all(
-      allFriendsData.map(async (friend: Friend) => {
-        const fr_response: Response = await fetch(
-          `${API_URL_BASE}/users/${friend.id}`
-        );
-        const friendData = await fr_response.json();
-        return friendData;
-      })
-    );
-
-    // Playlist data (Create, Delete playlist)
-    const pl_response: Response = await fetch(
-      `${API_URL_BASE}/users/${userID}/playlists`
-    );
-    const playlistData = await pl_response.json();
-
-    // Episode data (Add, Delete)
-    const episodesData = await Promise.all(
-      playlistData.map(async (playlist: Playlist) => {
-        const e_response: Response = await fetch(
-          `${API_URL_BASE}/users/${userID}/playlists/${playlist.name}/episodes`
-        );
-        const epData: Episode = await e_response.json();
-        return epData;
-      })
-    );
-    // log out data
-    console.log(`profile data:`);
-    console.log(profileData);
-    console.log(`friends:`);
-    console.log(friendsData);
-    console.log(`friend details:`);
-    console.log(friendsData);
-    console.log(`playlists:`);
-    console.log(playlistData);
-    console.log("episodes");
-    console.log(episodesData);
-  }
-  getUserInfo();
+    getUserInfo();
+  }, [loggedIn]);
   return (
     <>
       <div>
-        <h1>Profile page</h1>
+        <h1>{profile.username}</h1>
       </div>
       <div>Friends list</div>
+      <Friends />
       <div>Playlists</div>
-      <div></div>
+      <Playlists />
     </>
   );
 }
