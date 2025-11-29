74d53be0e180eb1adca3a78550ad33ae5c4d7ce4
jassem-razzaq <jassem.razzaq@gmail.com>
2025-11-29 16:23:18 -0500

    refactor: make usercard just for results

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import {
   Item,
   ItemActions,
@@ -9,50 +8,14 @@ import {
   ItemTitle,
 } from "@/components/ui/item";
 import minimalistAvatarM from "../assets/minimalistAvatarM.jpg";
-import { LoginContext } from "../contexts/LoginContext";
-import { Check, CircleX } from "lucide-react";
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
-
-type Friend = {
-  id: string;
-  date_added: string;
-  username: string;
-  first_name: string;
-  last_name: string;
-  bio: string;
-};
 
 type UserCardProps = {
-  mode?: "list" | "requests";
-  id: string;
   username: string;
-  first_name: string;
-  last_name: string;
   bio: string;
   onClick: () => void;
-  user: Friend;
-  onFriendAccept: (userID: string, urlID: string, name: Friend) => void;
-  onFriendReject: (userID: string, urlID: string, name: Friend) => void;
-  onFriendDelete: (name: Friend) => void;
 };
 
-export default function UserCard({
-  mode,
-  id,
-  username,
-  first_name,
-  last_name,
-  bio,
-  onClick,
-  user,
-  onFriendAccept,
-  onFriendReject,
-  onFriendDelete,
-}: UserCardProps) {
-  const { userID } = useContext(LoginContext);
-  const urlID = useParams().userID;
-
+export default function UserCard({ username, bio, onClick }: UserCardProps) {
   return (
     <div className="flex w-full max-w-lg flex-col gap-6 cursor-pointer">
       <Item variant="outline" onClick={onClick}>
@@ -66,52 +29,7 @@ export default function UserCard({
           <ItemTitle>{username}</ItemTitle>
           <ItemDescription className="break-all">{bio}</ItemDescription>
         </ItemContent>
-        <ItemActions>
-          {/* Friends list actions */}
-          {mode === "list" && userID == urlID && (
-            <Button
-              size="icon-sm"
-              variant="outline"
-              className="rounded-full"
-              aria-label="Remove friend"
-              onClick={(e) => {
-                e.stopPropagation();
-                onFriendDelete(user);
-              }}
-            >
-              <CircleX />
-            </Button>
-          )}
-          {/* Pending requests actions */}
-          {mode === "requests" && (
-            <div>
-              <Button
-                size="icon-sm"
-                variant="outline"
-                className="rounded-full"
-                aria-label="Accept friend"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onFriendAccept(user.id, userID, user);
-                }}
-              >
-                <Check />
-              </Button>
-              <Button
-                size="icon-sm"
-                variant="outline"
-                className="rounded-full"
-                aria-label="Reject friend"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onFriendReject(user.id, userID, user);
-                }}
-              >
-                <CircleX />
-              </Button>
-            </div>
-          )}
-        </ItemActions>
+        <ItemActions></ItemActions>
       </Item>
     </div>
   );
