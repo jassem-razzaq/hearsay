b289df8db9f130ae1a1b259b3b44c708d65ed428
jassem-razzaq <jassem.razzaq@gmail.com>
2025-12-02 19:38:37 -0500

    fix: friends list always open first time

--- frontend/src/components/FriendsList.tsx
+++ frontend/src/components/FriendsList.tsx
@@ -41,7 +41,6 @@ export default function FriendsList({
   useEffect(() => {
     function closeFriendsList() {
       if (open) setOpen(!open);
-      else setOpen(!open);
     }
     closeFriendsList();
   }, [urlID, userID]);
